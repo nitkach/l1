9f039e4f9a40381f1ae95ba5de474f4c985ab7ef
Nikita Tkachev <nikita.tkachev2001@gmail.com>
2024-09-19 12:37:38 +0300

    4, 5, 6

--- src/bin/l04.rs
+++ src/bin/l04.rs
@@ -1,19 +1,61 @@
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-    time::Duration,
-};
+use std::{thread, time::Duration};
 
 /// Implement permanent data recording in the channel (main stream).
 /// Implement a set of N workers that read arbitrary data from a channel
 /// and output it to stdout.
 /// It is necessary to be able to select the number of workers at the start.
 
-fn main() {
+async fn task_tokio_broadcast(workers: usize) {
+    let (sender, _) = tokio::sync::broadcast::channel::<i32>(16);
+    for worker_number in 1..=workers {
+        let receiver = sender.subscribe();
+        tokio::spawn(create_worker(worker_number, receiver));
+    }
+
+    let mut data = 0;
+    loop {
+        if sender.send(data).is_err() {
+            break;
+        }
+        data += 1;
+        tokio::time::sleep(Duration::from_millis(200)).await;
+        if data > 50 {
+            break;
+        }
+    }
+
+    drop(sender);
+
+    tokio::time::sleep(Duration::from_secs(2)).await;
+}
+
+async fn create_worker(worker_number: usize, mut receiver: tokio::sync::broadcast::Receiver<i32>) {
+    loop {
+        let message = receiver.recv().await;
+
+        match message {
+            Ok(num) => {
+                println!("Worker №{worker_number} received {num}");
+            }
+            Err(err) => {
+                println!("Worker №{worker_number} encountered error: {err}");
+                break;
+            }
+        };
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let workers = 5;
+    task_tokio_broadcast(workers).await;
+}
+
+fn task_crossbeam_channel() {
     let num_workers = 5;
     let (sender, receiver) = crossbeam_channel::unbounded::<i32>();
 
-    for worker_number in 0..num_workers {
+    for worker_number in 1..=num_workers {
         let receiver = receiver.clone();
         thread::spawn(move || loop {
             let message = receiver.recv();
@@ -29,40 +71,9 @@ fn main() {
 
     let mut data = 0;
     loop {
-        sender.send(data).unwrap();
-        data += 1;
-        thread::sleep(Duration::from_millis(200));
-        if data > 50 {
+        if sender.send(data).is_err() {
             break;
         }
-    }
-
-    drop(sender);
-
-    thread::sleep(Duration::from_secs(2));
-}
-
-fn foo() {
-    let num_workers = 20;
-    let (sender, receiver) = std::sync::mpsc::channel::<i32>();
-    let receiver = Arc::new(Mutex::new(receiver));
-
-    for worker_number in 0..num_workers {
-        let receiver = receiver.clone();
-        thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv();
-            if let Ok(num) = message {
-                println!("Worker {worker_number} received {num}");
-            } else {
-                println!("Channel has been closed, worker {worker_number} stopping");
-                break;
-            }
-        });
-    }
-
-    let mut data = 0;
-    loop {
-        sender.send(data).unwrap();
         data += 1;
         thread::sleep(Duration::from_millis(200));
         if data > 50 {
--- src/bin/l06.rs
+++ src/bin/l06.rs
@@ -1,1 +1,61 @@
-fn main() {}
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio_util::sync::CancellationToken;
+
+/// Develop a program that will sequentially send values ​​to a channel,
+/// and read from the other side of the channel.
+/// After N seconds, the program should terminate.
+
+#[tokio::main]
+async fn main() {
+    let working_time = 3;
+
+    let (sender, mut receiver) = tokio::sync::mpsc::channel::<i32>(16);
+    let cancellation_token = CancellationToken::new();
+
+    // spawn a task that sends values
+    {
+        let sender = sender.clone();
+        let cancellation_token = cancellation_token.clone();
+        let mut counter = 0;
+        tokio::spawn(async move {
+            loop {
+                if cancellation_token.is_cancelled() {
+                    println!("Cancellation token has been cancelled.");
+                    break;
+                }
+
+                if sender.send(counter).await.is_err() {
+                    println!("Receiver dropped");
+                    break;
+                };
+
+                counter += 1;
+                sleep(Duration::from_millis(50)).await;
+            }
+        })
+    };
+
+    // spawn a task that receives values
+    tokio::spawn(async move {
+        loop {
+            if let Some(num) = receiver.recv().await {
+                println!("Received: {num}");
+            } else {
+                println!("All senders dropped - channel is closed and more messages left.");
+                break;
+            }
+        }
+    });
+
+    // while sleep, the tasks are running and sending/receiving values
+    sleep(Duration::from_secs(working_time)).await;
+
+    drop(sender);
+    println!("Sender dropped");
+    sleep(Duration::from_millis(500)).await;
+
+    cancellation_token.cancel();
+
+    sleep(Duration::from_secs(1)).await;
+}
